410db95768e358474375c12748d8f177407f56d0
unknown <ralphfedericktan@gmail.com>
2024-10-26 17:09:22 -0500

    Fixed Booking Functionality

--- booking.cpp
+++
@@ -1,5 +0,0 @@
-// Booking.cpp
-#include "booking.h"
-
-Booking::Booking(std::string user, std::string e, std::string p, std::string checkIn, std::string checkOut, Room r)
-    : username(user), email(e), paymentInfo(p), checkInDate(checkIn), checkOutDate(checkOut), room(r) {}
