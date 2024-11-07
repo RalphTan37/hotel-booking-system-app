7681908194f5c86ee14a0272300884d168e16824
unknown <ralphfedericktan@gmail.com>
2024-11-07 11:09:42 -0600

    Created Database Connection in UserAccount File

--- userAccount/useraccount.cpp
+++ userAccount/useraccount.cpp
@@ -4,3 +4,16 @@
 
 using namespace std;
 
+MYSQL* connectDatabase() {
+    MYSQL* conn = mysql_init(0);
+    if (conn) {
+        //Will need to replace username and password with actual values
+        conn = mysql_real_connect(conn, "localhost:3306", "username", "password", "Hotel", 0, NULL, 0);
+        if (conn) {
+            cout << "Connected to the Hotel Database!" << endl;
+            return conn;
+        }
+        cout << "Hotel Database Connection Failed: " << mysql_error(conn) << endl;
+        return nullptr;
+    }
+}
