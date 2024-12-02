b9d99a239801853c9476291e973d788abc8a6681
Ralph Tan <ralphfedericktan@gmail.com>
2024-12-02 05:50:20 -0500

    Hotel DB Connection for Room Search

--- hotel_functionality/room.cpp
+++
@@ -1,4 +0,0 @@
-// Room.cpp
-#include "room.h"
-
-Room::Room(int num, std::string t, double p) : roomNumber(num), type(t), price(p), isAvailable(true) {}
---
+++ hotel_functionality/room_search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <mysql.h>
+
+using namespace std;
+
+// Connection with Hotel DB
+MYSQL* connectDatabase() {
+    MYSQL* conn = mysql_init(0);
+    if (!conn) {
+        cerr << "MySQL Initialization Failed" << endl;
+        return nullptr;
+    }
+
+    conn = mysql_real_connect(conn, "localhost", "username", "password", "Hotel", 3306, NULL, 0);
+    if (conn) {
+        cout << "Connected to the Hotel Database!" << endl;
+        return conn;
+    }
+
+    cerr << "Database Connection Failed: " << mysql_error(conn) << endl;
+    return nullptr;
+}
