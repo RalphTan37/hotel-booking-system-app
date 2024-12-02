5a327d5f88543c230e80473b31cb6ef3d843a60f
Ralph Tan <ralphfedericktan@gmail.com>
2024-12-02 08:27:24 -0500

    Added Booking Functionality

--- hotel_functionality/hotel_booking.cpp
+++ hotel_functionality/hotel_booking.cpp
@@ -48,3 +48,28 @@ bool isRoomAvailable(MYSQL* conn, int roomNumber) {
     mysql_free_result(result);
     return false;
 }
+
+// Booking a Room
+bool bookRoom(MYSQL* conn, int roomNumber, const string& customerName, const string& bookingDate) {
+    if (!isRoomAvailable(conn, roomNumber)) {
+        cout << "Room is not available for booking." << endl;
+        return false;
+    }
+
+    string query = "INSERT INTO Bookings (RoomNumber, CustomerName, BookingDate) VALUES (" +
+                   to_string(roomNumber) + ", '" + customerName + "', '" + bookingDate + "')";
+    if (mysql_query(conn, query.c_str())) {
+        cerr << "Booking Error: " << mysql_error(conn) << endl;
+        return false;
+    }
+
+    cout << "Room " << roomNumber << " successfully booked for " << customerName << " on " << bookingDate << endl;
+
+    string updateQuery = "UPDATE Rooms SET AvailabilityDate = '" + bookingDate + "' WHERE RoomNumber = " + to_string(roomNumber);
+    if (mysql_query(conn, updateQuery.c_str())) {
+        cerr << "Update Error: " << mysql_error(conn) << endl;
+        return false;
+    }
+
+    return true;
+}
